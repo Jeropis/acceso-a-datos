669d966b68c79916417904d4c2820db286f5c6ad
Jeropis <jerocaru@gmail.com>
2022-10-21 13:36:31 +0200

    añadidos metodos delete, deleteAll y findById

--- prZoologicoDAO/src/prZoologicoDAO/AnimalDAO.java
+++ prZoologicoDAO/src/prZoologicoDAO/AnimalDAO.java
@@ -2,49 +2,116 @@ package prZoologicoDAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public abstract class AnimalDAO {
 
 	private static Connection connection;
-	
+
 	public static void insertAnimal(Animal animal) {
 		connection = openConnection();
-		
+
 		String query = "insert into animales(nombre, habitat, peso_aproximado) values (?,?,?);";
-		
+
 		try {
 			PreparedStatement ps = connection.prepareStatement(query);
-			
+
 			ps.setString(1, animal.getNombre());
 			ps.setString(2, animal.getHabitat());
 			ps.setDouble(3, animal.getPeso_aproximado());
-			
+
 			ps.executeUpdate();
-			
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		closeConnection();
-		
+
 	}
-	
+
+	public static Animal findById(int id) {
+
+		connection = openConnection();
+		Animal a = null;
+		String query = "select * from animales where id = ?";
+
+		Statement statement;
+		try {
+			PreparedStatement ps = connection.prepareStatement(query);
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				String nombre = rs.getString("nombre");
+				String habitat = rs.getString("habitat");
+				double peso = rs.getDouble("peso_aproximado");
+
+				a = new Animal(id, nombre, habitat, peso);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return a;
+
+	}
+
+	public static void deleteAnimalByNombre(String nombre) {
+		connection = openConnection();
+
+		String query = "delete from animales where nombre = '" + nombre + "'";
+
+		try {
+
+			Statement statement = connection.createStatement();
+			statement.executeUpdate(query);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		closeConnection();
+
+	}
+
+	public static void deleteAllAnimal() {
+		connection = openConnection();
+
+		String query = "delete from animales";
+
+		try {
+
+			PreparedStatement preparedStatement = connection.prepareStatement(query);
+			preparedStatement.executeUpdate(query);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		closeConnection();
+
+	}
+
 	private static Connection openConnection() {
 		DatabaseConnection dbConnection = new DatabaseConnection();
 		connection = dbConnection.getConnection();
 		return connection;
 	}
-	
+
 	private static void closeConnection() {
 		try {
 			connection.close();
 			connection = null;
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 }
--- prZoologicoDAO/src/prZoologicoDAO/Principal.java
+++ prZoologicoDAO/src/prZoologicoDAO/Principal.java
@@ -4,11 +4,18 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		Animal a1 = new Animal("Ardilla", "Bosque", 0.1);
+//		Animal a1 = new Animal("Ardilla", "Bosque", 0.1);
+//		AnimalDAO.insertAnimal(a1);
+//		Animal a2 = new Animal("Perro", "Domestico", 0.2);
+//		AnimalDAO.insertAnimal(a2);
 		
-		AnimalDAO.insertAnimal(a1);
+//		AnimalDAO.deleteAllAnimal();
 		
+		Animal buscador = AnimalDAO.findById(1);
 		
+		System.out.println(buscador);
+		
+
 	}
 	
 }
