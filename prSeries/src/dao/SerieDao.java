ea6d99b7114f966e13a8e81fd2d4273f9ab29e64
Jeropis <jerocaru@gmail.com>
2022-10-28 13:32:43 +0200

    Primer commit

---
+++ prSeries/src/dao/SerieDao.java
@@ -0,0 +1,76 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import pojo.Serie;
+import util.DatabaseConnection;
+
+public class SerieDao implements Dao<Serie> {
+
+	private static Connection connection;
+
+	public SerieDao() {
+		
+	}
+	
+	@Override
+	public ArrayList<Serie> buscarTodos() {
+
+		return null;
+	}
+
+	@Override
+	public Serie buscarPorId(int i) {
+
+		return null;
+	}
+
+	@Override
+	public void insertar(Serie serie) {
+
+		connection = openConnection();
+
+		String query = "insert into series(titulo, edad, plataforma) values (?,?,?)";
+
+		try {
+			PreparedStatement ps = connection.prepareStatement(query);
+			ps.setString(1, serie.getTitulo());
+			ps.setInt(2, serie.getEdad());
+			ps.setString(3, serie.getPlataforma());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		closeConnection();
+	}
+
+	@Override
+	public void modificar(Serie t) {
+
+	}
+
+	@Override
+	public void borrar(Serie t) {
+
+	}
+
+	private static Connection openConnection() {
+		DatabaseConnection dbConnection = new DatabaseConnection();
+		connection = dbConnection.getConnection();
+		return connection;
+	}
+
+	private static void closeConnection() {
+		try {
+			connection.close();
+			connection = null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ prSeries/src/dao/TemporadaDao.java
@@ -0,0 +1,39 @@
+package dao;
+
+import java.util.ArrayList;
+
+import pojo.Temporada;
+
+public class TemporadaDao implements Dao<Temporada> {
+
+	@Override
+	public ArrayList<Temporada> buscarTodos() {
+
+		return null;
+	}
+
+	@Override
+	public Temporada buscarPorId(int i) {
+
+		return null;
+	}
+
+	@Override
+	public void insertar(Temporada t) {
+
+		
+	}
+
+	@Override
+	public void modificar(Temporada t) {
+
+		
+	}
+
+	@Override
+	public void borrar(Temporada t) {
+
+		
+	}
+
+}
